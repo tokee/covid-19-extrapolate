6c0f3d8948a1ffcac4c7f572d730bfaaff244131
Toke Eskildsen <te@ekot.dk>
2020-04-05 21:03:51 +0200

    Bugfix: Downloading of data was commented out

--- get_data.sh
+++ get_data.sh
@@ -2,7 +2,7 @@
 
 : ${COUNTRY:="Denmark"}
 
-#curl -s 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv' > hopkins-deaths.csv
+curl -s 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv' > hopkins-deaths.csv
 
 head -n 1 hopkins-deaths.csv | sed 's/.*Long,//' | tr ',' '\n' | xargs -I {} -n 1 bash -c "date -d '{} -1 day' +%Y-%m-%d" > ${COUNTRY}-deaths-dates.dat
 grep ${COUNTRY} hopkins-deaths.csv | grep "^,${COUNTRY}" |  sed "s/,${COUNTRY},[-0-9.]*,[-0-9.]*,//" | tr ',' '\n' > ${COUNTRY}-deaths-numbers.dat
