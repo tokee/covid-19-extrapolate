a916d12de4bc7aa1891c85285016da4192fadcb4
Toke Eskildsen <te@ekot.dk>
2020-04-05 20:19:41 +0200

    Made country an option

--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-curl -s 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv' > hopkins-deaths.csv
+: ${COUNTRY:="Denmark"}
 
-head -n 1 hopkins-deaths.csv | sed 's/.*Long,//' | tr ',' '\n' | xargs -I {} -n 1 bash -c "date -d '{} -1 day' +%Y-%m-%d" > denmark-deaths-dates.dat
-grep Denmark hopkins-deaths.csv | grep '^,Denmark' |  sed 's/,Denmark,56.2639,9.5018,//' | tr ',' '\n' > denmark-deaths-numbers.dat
-paste denmark-deaths-dates.dat denmark-deaths-numbers.dat | grep -v '.*[^0-9]0$' > denmark-deaths.dat
+#curl -s 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv' > hopkins-deaths.csv
+
+head -n 1 hopkins-deaths.csv | sed 's/.*Long,//' | tr ',' '\n' | xargs -I {} -n 1 bash -c "date -d '{} -1 day' +%Y-%m-%d" > ${COUNTRY}-deaths-dates.dat
+grep ${COUNTRY} hopkins-deaths.csv | grep "^,${COUNTRY}" |  sed "s/,${COUNTRY},[-0-9.]*,[-0-9.]*,//" | tr ',' '\n' > ${COUNTRY}-deaths-numbers.dat
+paste ${COUNTRY}-deaths-dates.dat ${COUNTRY}-deaths-numbers.dat | grep -v '.*[^0-9]0$' > ${COUNTRY}-deaths.dat
